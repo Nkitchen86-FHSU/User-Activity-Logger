d0f7fa776a6539829af77d0edee50c3c772d7a96
Nicholas Kitchen <njkitchen@mail.fhsu.edu>
2025-09-27 14:02:06 -0500

    Need to restore

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import fs from 'fs';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
@@ -10,6 +11,13 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
 
 let logs = [];
+let allLogs = [];
+const logFilePath = path.join(__dirname, 'logs.txt');
+
+if (!fs.existsSync(logFilePath)) {
+    fs.writeFileSync(logFilePath, '');
+    console.log('Log file created!');
+}
 
 // Endpoint to receive logs
 app.post('/log', (req, res) => {
@@ -21,11 +29,28 @@ app.post('/log', (req, res) => {
 // Periodically process logs in batches
 setInterval(() => {
     if (logs.length === 0) return console.log('No Activity...');
+    allLogs.push(...logs);
     const summary = aggregateLogs(logs);
     console.log('--- Log Batch Summary ---');
     console.log(summary);
+    try {
+        fs.writeFileSync(logFilePath, JSON.stringify(allLogs, null, 2));
+        console.log('Logs added to logs.txt successfully');
+    } catch (err) {
+        console.log(`Error writing file: ${err}`);
+    }
     logs = [];
-}, 10 * 1000);  // Process every 30 seconds
+}, 10 * 1000);  // Process every 10 seconds
+
+// Periodically clear the logs.txt file
+setInterval(() => {
+    try {
+        fs.writeFileSync(logFilePath, '');
+        console.log('logs.txt cleared');
+    } catch (err) {
+        console.log(`Error writing file: ${err}`);
+    }
+}, 60 * 1000); // Reset every 60 seconds
 
 function aggregateLogs(events) {
     const counts = {};
